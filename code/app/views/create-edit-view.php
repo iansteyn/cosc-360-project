ee2a930c8e2af6c79fbc12e6a8e2ebb0b66f2f90
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-04-09 20:09:23 -0700

    Add form-validation.js to create-edit-view.php

--- code/app/views/create-edit-view.php
+++ code/app/views/create-edit-view.php
@@ -31,7 +31,7 @@
     echo generateDocumentHead(
         $pageTitle,
         ['forms.css', 'create-edit.css'],
-        ['create-edit.js']
+        ['create-edit.js', 'form-validation.js']
     );
 ?>
 
